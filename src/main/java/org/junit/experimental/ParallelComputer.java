05de2855f89be78aae4652655bc577a6a24ebc50
Lorenzo Nosotti <lorenzo.nosotti.sa@everis.com>
2019-12-24 15:14:46 +0100

    fix 2 - interruzione thread

--- src/main/java/org/junit/experimental/ParallelComputer.java
+++ src/main/java/org/junit/experimental/ParallelComputer.java
@@ -43,6 +43,7 @@ public class ParallelComputer extends Computer {
                         fService.shutdown();
                         fService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
                     } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();// fix 2
                         e.printStackTrace(System.err);
                     }
                 }
